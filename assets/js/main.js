5f7b66a2fc26de33edbd89614698c6985d8596ce
Raul Farias S <rfarias@chattigo.com>
2022-04-29 01:17:55 -0400

    add hero and resume

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const typed = new Typed('.professions', {
+  strings: [
+    'Full Stack JavaScript Developer.',
+    'Ing. en Informática.',
+    'Scrum Master.',
+    'Scrum Product Owner.',
+    'Scrum Developer.',
+    'Admin. de Sistemas.',
+    'Tec. en telecomunicaciones.'
+  ],
+  typeSpeed: 90,
+  backSpeed: 15,
+  loop: true
+});
