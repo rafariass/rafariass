9ae55a69c30b469d43814f12a0b34dd06a899dfa
Raul Farias S <rfarias@chattigo.com>
2022-04-30 23:49:10 -0400

    add section certificaciones and contactame

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Typed words
 const typed = new Typed('.professions', {
   strings: [
     'Full Stack JavaScript Developer.',
@@ -12,3 +13,48 @@ const typed = new Typed('.professions', {
   backSpeed: 15,
   loop: true
 });
+
+// Scroll-Top
+window.addEventListener('scroll', () => {
+  const scroll = document.querySelector('.scrollUp');
+  scroll.classList.toggle('active', window.scrollY > 100);
+});
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Tooltips
+const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+tooltips.forEach((t) => new bootstrap.Tooltip(t));
+
+// Google Recaptcha v3 (Form contact)
+function onSubmit(token) {
+  Swal.fire({
+    position: 'center',
+    icon: 'warning',
+    text: 'Temporalmente este formulario esta fuera de servicio, por favor intentalo mas tarde.',
+    showConfirmButton: true,
+    confirmButtonText: 'Cerrar',
+    confirmButtonColor: '#3085d6',
+    timer: 4000,
+    showClass: { popup: 'animate__animated animate__fadeInDown' },
+    hideClass: { popup: 'animate__animated animate__fadeOutUp' }
+  });
+  // document.getElementById('formulario').submit();
+}
+
+// Animated RRSS icons
+const icons = document.querySelectorAll('.rrss-animated');
+icons.forEach(i => {
+  i.addEventListener('mouseover', ({ target }) => target.classList.add('animate__animated', 'animate__heartBeat'));
+  i.addEventListener('mouseout', ({ target }) => target.classList.remove('animate__animated', 'animate__heartBeat'));
+});
+
+// Navbar Collapse
+const navbar = document.querySelector('.navbar-collapse');
+const bsCollapse = new bootstrap.Collapse(navbar, { toggle: false });
+const navbarCollapse = () => bsCollapse.hide();
